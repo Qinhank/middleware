128ffa55a91e7203133b796cb4b40b0d35d15018
Bepal.er <348118672@qq.com>
2020-07-01 15:10:36 +0800

    'u'

--- routes/android/index.js
+++ routes/android/index.js
@@ -5,7 +5,7 @@
  * @Desc: 安卓APK版本管理项目接口配置 
  */
 
-const { sendRequest, back2json } = require('../../core')
+const { sendRequest, back2json, address } = require('../../core')
 
 module.exports = {
   // address: 'http://webapi.bk.apal.com.cn',
@@ -19,21 +19,23 @@ module.exports = {
         const { data: apks } = req.query
         const apkArr = apks.split(',')
         let obj = {}
-        const data = await Promise.all(apkArr.map(_ => {
-          return sendRequest('http://192.168.0.253:84/api/v1.0/androidVersion/getVersionByApk', 'GET', {}, {
-            params: {
-              apk: _
-            },
-            headers: {
-              [token]: req.headers[token]||'',
-              'content-type': 'application/json'
-            }
-          })
-        }))
-        if ( data && data.length ) {
-          data.map(( _, i ) => {
-            obj[apkArr[i]] = _.data
-          })
+        if ( apks && apkArr && apkArr.length) {
+          const data = await Promise.all(apkArr.map(_ => {
+            return sendRequest( address + '/api/v1.0/androidVersion/getVersionByApk', 'GET', {}, {
+              params: {
+                apk: _
+              },
+              headers: {
+                [token]: req.headers[token]||'',
+                'content-type': 'application/json'
+              }
+            })
+          }))
+          if ( data && data.length ) {
+            data.map(( _, i ) => {
+              obj[apkArr[i]] = _.data
+            })
+          }
         }
         
         return back2json({ success: true, msg: '请求数据成功', data: obj, code: 200 })
